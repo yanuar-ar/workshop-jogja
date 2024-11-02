9a7356aaf0b45b29512e739298151647ec660d9e
Yanzero <yanuar.ar@gmail.com>
2024-11-02 11:13:20 +0700

    init

--- src/Counter.sol
+++ src/Counter.sol
@@ -3,6 +3,30 @@ pragma solidity ^0.8.13;
 
 contract Counter {
     uint256 public number;
+    uint256 constant public totalSupply = 1000;
+
+    uint256 public price;
+
+    address public owner;
+
+    constructor(uint256 _price) {
+        owner = msg.sender;
+        price = _price;
+    }
+
+    function setPrice(uint256 newPrice) public {
+        require(msg.sender == owner, "Only owner can set price");
+        price = newPrice;
+    }
+
+    function setOwner(address newOwner) public {
+        require(msg.sender == owner, "Only owner can set owner");
+        owner = newOwner;
+    }
+
+    function add(uint256 a, uint256 b) public pure returns (uint256) {
+        return a + b;
+    }
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
@@ -12,3 +36,5 @@ contract Counter {
         number++;
     }
 }
+
+
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -8,7 +8,7 @@ contract CounterTest is Test {
     Counter public counter;
 
     function setUp() public {
-        counter = new Counter();
+        counter = new Counter(100);
         counter.setNumber(0);
     }
 
@@ -20,5 +20,34 @@ contract CounterTest is Test {
     function testFuzz_SetNumber(uint256 x) public {
         counter.setNumber(x);
         assertEq(counter.number(), x);
+    }
+
+    function test_TotalSupply() public {
+        console.log(counter.totalSupply());
+        assertEq(counter.totalSupply(), 1000);
+    }
+
+    function test_SetPrice() public {
+        console.log(counter.owner());
+        counter.setPrice(100);
+        assertEq(counter.price(), 100);
+    }
+
+    function test_Add() public {
+        assertEq(counter.add(1, 2), 3);
+    }
+
+    function test_SetOwner() public {
+        address alice = address(1);
+        counter.setOwner(alice);
+        assertEq(counter.owner(), alice);
+
+        // ubah price dgn owner yang baru
+        vm.expectRevert("Only owner can set price");
+        counter.setPrice(200);
+
+        vm.prank(alice);
+        counter.setPrice(300);
+        assertEq(counter.price(), 300);
     }
 }
